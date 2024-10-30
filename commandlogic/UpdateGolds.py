666365bca1b235b6c46723fa2c12edcd56120b2d
unknown <williamcarter808@gmail.com>
2024-10-31 01:45:20 +1000

    Fixed bug with comgold activity log update being sent when a tied cgold is deleted

--- commandlogic/UpdateGolds.py
+++ commandlogic/UpdateGolds.py
@@ -44,19 +44,17 @@ async def Updategolds(command: interactions.Extension, ctx: interactions.SlashCo
     await ctx.send(f"```ansi\nUpdated golds! Your new sum of best is {newSobFormatted} ({UI.differences.colourDifference(difference)})!```")
     await activityFeed(command, userObj, categoryObj, oldComgolds, newComgolds)
 
-
-
     
 async def activityFeed(command: interactions.Extension, userObj: User.User, categoryObj: Category.Category, oldComgolds: list, newComgolds: list):
     updates = []
     for index, gold in enumerate(oldComgolds):
-        if gold[1] != newComgolds[index][1]:
-            if gold[1] > newComgolds[index][1]:
+        if gold[1] != newComgolds[index][1]: # check if the gold has changed
+            if gold[1] > newComgolds[index][1]: # make sure that it's faster (a rollback shouldn't send a notif)
                 updates.append(
                     f"{userObj.name} beat the community gold for {categoryObj.name.title()} {gold[0].name} with a time of {UI.durations.formatted(newComgolds[index][1])}!"
                 )
 
-        elif len(gold[2]) != len(newComgolds[index][2]):
+        elif len(gold[2]) < len(newComgolds[index][2]): # check if a new person has the same gold
             updates.append(
                 f"{userObj.name} tied the community gold for {categoryObj.name.title()} {gold[0].name} with a time of {UI.durations.formatted(newComgolds[index][1])}!"
             )
