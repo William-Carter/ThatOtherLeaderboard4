44793dc3b973068b6790961470e9a1295fe20f23
unknown <williamcarter808@gmail.com>
2024-11-11 09:53:31 +1000

    Added comment in setname discord function

--- discordFunctions/SetName.py
+++ discordFunctions/SetName.py
@@ -2,5 +2,5 @@ import interactions
 import tol
 
 async def updateUserNickname(user: interactions.Member, newName: str):
-    if not user.has_role(tol.adminRole):
+    if not user.has_role(tol.adminRole): # bot doesn't (and can't) have permissions to edit admin nicknames
         await user.edit_nickname(newName)
