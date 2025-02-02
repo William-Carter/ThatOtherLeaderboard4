97d3becf39d8bdfad5fe3493d0c688e5e8fd90ed
unknown <williamcarter808@gmail.com>
2025-02-03 09:45:04 +1000

    Restricted stop command to home guild

--- commands/Stop.py
+++ commands/Stop.py
@@ -1,10 +1,12 @@
 import interactions
+import tol
 import sys
 
 class Stop(interactions.Extension):
     @interactions.slash_command(
         name="stop",
         description="Stop the bot",
+        scopes=[tol.homeGuild]
     )
 
     async def stop(self, ctx: interactions.SlashContext):
